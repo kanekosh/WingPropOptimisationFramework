49b7cbe47caa8d65850a48b76180ee2a0ec530b9
jexalto99 <48797422+jexalto@users.noreply.github.com>
2023-08-03 11:11:49 -0400

    examples|added example, not working tho

--- examples/analysis/simple_dual_prop.py
+++ examples/analysis/simple_dual_prop.py
@@ -1,31 +1,60 @@
 # --- Built-ins ---
 
 # --- Internal ---
-from src.base import WingPropInfo, WingInfo
-from src.utils.meshing import meshing
-from openaerostruct.geometry.utils import generate_mesh
-from openaerostruct.integration.aerostruct_groups import AerostructGeometry, AerostructPoint
-from openaerostruct.utils.constants import grav_constant
+from src.base import WingPropInfo, WingInfo, PropInfo
+from src.integration.model_coupling import WingSlipstreamProp
 
 # --- External ---
 import numpy as np
 import openmdao.api as om
 
-class SimpleDualProp(om.group):
-    def setup(self):
-        # Add problem information as an independent variables component
-        indep_var_comp = om.IndepVarComp()
-        indep_var_comp.add_output("v", val=100., units="m/s")
-        indep_var_comp.add_output("alpha", val=5.0, units="deg")
-        indep_var_comp.add_output("Mach_number", val=0.84)
-        indep_var_comp.add_output("re", val=1.0e6, units="1/m")
-        indep_var_comp.add_output("rho", val=0.38, units="kg/m**3")
-        indep_var_comp.add_output("CT", val=grav_constant * 17.0e-6, units="1/s")
-        indep_var_comp.add_output("R", val=11.165e6, units="m")
-        indep_var_comp.add_output("W0", val=0.4 * 3e5, units="kg")
-        indep_var_comp.add_output("speed_of_sound", val=295.4, units="m/s")
-        indep_var_comp.add_output("load_factor", val=1.0)
-        indep_var_comp.add_output("empty_cg", val=np.zeros((3)), units="m")
+spanwise_discretisation_propeller_BEM = 10
 
-        indep_var_comp.add_output("velocity_distribution", val=velocity_distribution, units="m/s")
-        indep_var_comp.add_output("rethorst_correction", val=correction) #np.zeros( ((y_panels-1), (y_panels-1)) ) )
+prop1 = PropInfo(label='Prop1',
+                 prop_location=-1.,
+                 nr_blades=4,
+                 rot_rate=500.,
+                 chord=np.ones(spanwise_discretisation_propeller_BEM, 
+                                order='F'),
+                 twist=np.ones(spanwise_discretisation_propeller_BEM, 
+                                order='F'),
+                 span=np.array(np.linspace(0.1, 1.0, spanwise_discretisation_propeller_BEM), 
+                                order='F')
+                 )
+
+prop2 = PropInfo(label='Prop1',
+                 prop_location=-1.,
+                 nr_blades=4,
+                 rot_rate=500.,
+                 chord=np.ones(spanwise_discretisation_propeller_BEM, 
+                                order='F'),
+                 twist=np.ones(spanwise_discretisation_propeller_BEM, 
+                                order='F'),
+                 span=np.array(np.linspace(0.1, 1.0, spanwise_discretisation_propeller_BEM), 
+                                order='F')
+                 )
+
+propellers = [prop1, prop2]
+wingpropinfo = WingPropInfo(nr_props=2,
+                            spanwise_discretisation_wing=30,
+                            spanwise_discretisation_propeller=5,
+                            spanwise_discretisation_propeller_BEM=spanwise_discretisation_propeller_BEM,
+                            propeller=propellers
+                            )
+
+class WingSlipstreamPropAnalysis(om.Group):
+    def initialize(self):
+        self.declare.options('WingPropInfo', default=WingPropInfo())
+    
+    def setup(self):
+        self.add_subsystem('PropellerSlipstreamWingModel',
+                           subsys=WingSlipstreamPropAnalysis(WingPropInfo=wingpropinfo))
+        
+    def configure(self):
+        # Empty because we do analysis
+        ...
+
+prob = om.Problem()
+prob.model = WingSlipstreamPropAnalysis(WingPropInfo=wingpropinfo)
+prob.setup()
+prob.run_model()
